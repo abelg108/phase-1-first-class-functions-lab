7920d3b19ac8e47a2d173c9d74dbd5c31550eee8
Abel Bokure <abel.ngt@gmail.com>
2022-05-24 22:45:51 -0400

    commit tree 695f8a5772fd046c25e0c8f4b37f3299f7672fd9

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers (){['Antonia', 'Nuru', 'Amari', 'Mo']
+    return(['Antonia', 'Nuru'])
+}
+function returnLastTwoDrivers () {['Antonia', 'Nuru','Amari','Mo']
+    return (['Amari','Mo'])
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (fareMultiplier) {
+     return function (value) {
+         return fareMultiplier * value
+     }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers (arrayOfDrivers,func){
+    return func(arrayOfDrivers)
+}
